e9bf002a1310d6b45456761ab8d51114eb99cf84
ruitobias-eng <ruitobiascarvalho@hotmail.com>
2025-08-21 17:44:16 -0300

    primeiro commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import '../styles/neumorphism.css';
+import heroImage from '../assets/images/hero_image.jpg';
 
 interface HeroProps {
   title: string;
@@ -12,7 +13,6 @@ const Hero = ({ title, subtitle, ctaText, ctaLink }: HeroProps) => {
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    // Trigger animations after component mounts
     setLoaded(true);
   }, []);
 
@@ -29,7 +29,7 @@ const Hero = ({ title, subtitle, ctaText, ctaLink }: HeroProps) => {
         <div className="flex flex-col md:flex-row items-center">
           {/* Text content */}
           <div className="w-full md:w-1/2 mb-10 md:mb-0">
-            <h1 
+            <h1
               className={`text-4xl md:text-5xl lg:text-6xl font-bold mb-6 ${
                 loaded ? 'nm-animate-left' : 'opacity-0'
               }`}
@@ -39,62 +39,69 @@ const Hero = ({ title, subtitle, ctaText, ctaLink }: HeroProps) => {
               <br />
               <span className="text-gray-800">{title}</span>
             </h1>
-            
-            <p 
+
+            <p
               className={`text-xl text-gray-600 mb-8 max-w-lg ${
                 loaded ? 'nm-animate-left nm-delay-200' : 'opacity-0'
               }`}
             >
               {subtitle}
             </p>
-            
+
             <div className={loaded ? 'nm-animate-up nm-delay-300' : 'opacity-0'}>
-              <a 
-                href={ctaLink} 
-                className="nm-button nm-button-primary inline-block"
-              >
+              <a href={ctaLink} className="nm-button nm-button-primary inline-block">
                 {ctaText}
               </a>
             </div>
           </div>
-          
+
           {/* Hero image/graphic */}
           <div className="w-full md:w-1/2">
-            <div 
-              className={`nm-card p-8 ${
-                loaded ? 'nm-animate-right nm-delay-200' : 'opacity-0'
-              }`}
-            >
-              <div className="relative">
-                {/* Placeholder for hero image */}
-                <div className="aspect-video bg-gradient-to-br from-gray-100 to-gray-200 rounded-lg flex items-center justify-center">
-                  <div className="nm-icon w-24 h-24 flex items-center justify-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 text-yellow-500" viewBox="0 0 20 20" fill="currentColor">
-                      <path fillRule="evenodd" d="M3 5a2 2 0 012-2h10a2 2 0 012 2v8a2 2 0 01-2 2h-2.22l.123.489.804.804A1 1 0 0113 18H7a1 1 0 01-.707-1.707l.804-.804L7.22 15H5a2 2 0 01-2-2V5zm5.771 7H5V5h10v7H8.771z" clipRule="evenodd" />
-                    </svg>
-                  </div>
-                </div>
-                
-                {/* Floating elements */}
-                <div 
-                  className={`absolute -top-4 -left-4 nm-icon bg-yellow-500 ${
-                    loaded ? 'nm-animate-scale nm-delay-400' : 'opacity-0'
-                  }`}
+            <div className={`nm-card p-8 relative ${loaded ? 'nm-animate-right nm-delay-200' : 'opacity-0'}`}>
+              <div className="aspect-video bg-gradient-to-br from-gray-100 to-gray-200 rounded-lg flex items-center justify-center">
+                <div
+                  className="nm-icon w-32 h-32 overflow-hidden rounded-full flex items-center justify-center"
+                  style={{ transform: 'scale(1.01139) rotate(1.13927deg)' }}
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" viewBox="0 0 20 20" fill="currentColor">
-                    <path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z" />
-                  </svg>
+                  <img src={heroImage} alt="Imagem principal" className="w-full h-full object-cover" />
                 </div>
-                
-                <div 
-                  className={`absolute -bottom-4 -right-4 nm-icon bg-green-500 ${
-                    loaded ? 'nm-animate-scale nm-delay-500' : 'opacity-0'
-                  }`}
+              </div>
+
+              {/* Floating icons */}
+              <div
+                className={`absolute -top-4 -left-4 nm-icon bg-yellow-500 flex items-center justify-center ${
+                  loaded ? 'nm-animate-scale nm-delay-400' : 'opacity-0'
+                }`}
+                style={{ width: '1.5rem', height: '1.5rem' }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-white"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z" clipRule="evenodd" />
-                  </svg>
-                </div>
+                  <path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z" />
+                </svg>
+              </div>
+
+              <div
+                className={`absolute -bottom-4 -right-4 nm-icon bg-green-500 flex items-center justify-center ${
+                  loaded ? 'nm-animate-scale nm-delay-500' : 'opacity-0'
+                }`}
+                style={{ width: '1.5rem', height: '1.5rem' }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-white"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z"
+                    clipRule="evenodd"
+                  />
+                </svg>
               </div>
             </div>
           </div>
