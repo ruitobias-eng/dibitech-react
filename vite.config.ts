75958bed12648d2eb1fb2233f66e41476e241467
ruitobias-eng <ruitobiascarvalho@hotmail.com>
2025-08-20 23:25:09 -0300

    primeiro commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
-import { defineConfig, loadEnv } from "vite"
+import { defineConfig } from "vite"
 
 export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current directory
-    const env = loadEnv(mode, process.cwd(), "")
-  
   return {
     plugins: [react()],
     resolve: {
@@ -13,23 +10,20 @@ export default defineConfig(({ mode }) => {
         "@": path.resolve(__dirname, "./src"),
       },
     },
-    base: mode === 'production' ? "/dibitech-react/" : "/",
+    base: mode === "production" ? "/dibitech-react/" : "/",
     build: {
       outDir: "dist",
       sourcemap: false,
-      // Ensure build output is clean
-      emptyOutDir: true
+      emptyOutDir: true,
     },
-    // Optional: Preview configuration
     preview: {
       port: 4173,
       strictPort: true,
     },
-    // Optional: Development server configuration
     server: {
       port: 3000,
       strictPort: true,
       host: true,
-    }
+    },
   }
 })
